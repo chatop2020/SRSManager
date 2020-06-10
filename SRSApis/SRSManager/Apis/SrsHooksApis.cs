6c1fea9c1b112f1c3848faa4529f55b29ec90e79
qiuzhouwei <chatop@qq.com>
2020-06-10 12:32:30 +0800

    修改http_hookapi中的数据库操作逻辑

--- SRSApis/SRSManager/Apis/SrsHooksApis.cs
+++ SRSApis/SRSManager/Apis/SrsHooksApis.cs
@@ -85,16 +85,66 @@ namespace SrsApis.SrsManager.Apis
         /// <returns></returns>
         private static string getMonitorIpAddressFromStream(string stream)
         {
-           var ret= OrmService.Db.Select<Client>().Where(x => x.Stream!.Trim().Equals(stream.Trim()) && x.ClientType==ClientType.Monitor).First();
-           if (ret != null)
-           {
-               return ret.MonitorIp!;
-           }
+            var ret = OrmService.Db.Select<Client>()
+                .Where(x => x.Stream!.Trim().Equals(stream.Trim()) && x.ClientType == ClientType.Monitor).First();
+            if (ret != null)
+            {
+                return ret.MonitorIp!;
+            }
 
-           return "unknow";
+            return "unknow";
         }
 
         /// <summary>
+        /// 客户端播放时
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public static bool OnPlay(Client client)
+        {
+            lock (Common.LockObj)
+            {
+                if (client != null && client.Client_Id != null && !string.IsNullOrEmpty(client.Device_Id))
+                {
+                    try
+                    {
+                        var ret = OrmService.Db.Update<Client>().Set(x => x.ClientType, ClientType.User)
+                            .Set(x => x.IsOnline, true).Set(x => x.IsPlay, true).Set(x => x.Param, client.Param)
+                            .Set(x => x.Stream, client.Stream).Set(x => x.UpdateTime, client.UpdateTime)
+                            .Set(x => x.PageUrl, client.PageUrl).Set(x => x.MonitorIp,
+                                getMonitorIpAddressFromStream(client.Stream!))
+                            .Where(x => x.Client_Id == client.Client_Id &&
+                                        x.Device_Id!.Trim() == client.Device_Id.Trim())
+                            .ExecuteAffrows();
+                        if (ret <= 0)
+                        {
+                            client.ClientType = ClientType.User;
+                            client.IsOnline = true;
+                            client.IsPlay = true;
+                            client.MonitorIp = getMonitorIpAddressFromStream(client.Stream!);
+                            var retInsert = OrmService.Db.Insert(client).ExecuteAffrows();
+                            if (retInsert > 0)
+                            {
+                                return true;
+                            }
+
+                            return false;
+                        }
+
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return false;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        /*/// <summary>
         /// 客户端播放时
         /// </summary>
         /// <param name="client"></param>
@@ -143,9 +193,55 @@ namespace SrsApis.SrsManager.Apis
 
                 return false;
             }
-        }
+        }*/
 
         /// <summary>
+        /// 客户端停止播放时
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public static bool OnStop(Client client)
+        {
+            lock (Common.LockObj)
+            {
+                if (client != null && !string.IsNullOrEmpty(client.Device_Id) && client.Client_Id != null)
+                {
+                    try
+                    {
+                        var ret = OrmService.Db.Update<Client>().Set(x => x.ClientType, ClientType.User)
+                            .Set(x => x.IsOnline, true).Set(x => x.IsPlay, false)
+                            .Set(x => x.UpdateTime, client.UpdateTime)
+                            .Where(x => x.Client_Id == client.Client_Id &&
+                                        x.Device_Id!.Trim() == client.Device_Id.Trim())
+                            .ExecuteAffrows();
+                        if (ret <= 0)
+                        {
+                            client.ClientType = ClientType.User;
+                            client.IsOnline = true;
+                            client.IsPlay = false;
+                            var retInsert = OrmService.Db.Insert(client).ExecuteAffrows();
+                            if (retInsert > 0)
+                            {
+                                return true;
+                            }
+
+                            return false;
+                        }
+
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return false;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        /*/// <summary>
         /// 客户端停止播放时
         /// </summary>
         /// <param name="client"></param>
@@ -193,10 +289,57 @@ namespace SrsApis.SrsManager.Apis
 
                 return false;
             }
-        }
-
+        }*/
 
         /// <summary>
+        /// 摄像头推流的时候
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public static bool OnPublish(Client client)
+        {
+            lock (Common.LockObj)
+            {
+                if (client != null && !string.IsNullOrEmpty(client.Device_Id) && client.Client_Id != null)
+                {
+                    try
+                    {
+                        var ret = OrmService.Db.Update<Client>().Set(x => x.ClientType, ClientType.Monitor)
+                            .Set(x => x.IsOnline, true).Set(x => x.HttpUrl, client.HttpUrl)
+                            .Set(x => x.Param, client.Param)
+                            .Set(x => x.Stream, client.Stream).Set(x => x.UpdateTime, client.UpdateTime)
+                            .Set(x => x.ClientType, ClientType.Monitor)
+                            .Where(x => x.Client_Id == client.Client_Id &&
+                                        x.Device_Id!.Trim() == client.Device_Id.Trim())
+                            .ExecuteAffrows();
+                        if (ret <= 0)
+                        {
+                            client.ClientType = ClientType.Monitor;
+                            client.MonitorType = MonitorType.Unknow;
+                            client.IsOnline = true;
+                            var retInsert = OrmService.Db.Insert(client).ExecuteAffrows();
+                            if (retInsert > 0)
+                            {
+                                return true;
+                            }
+
+                            return false;
+                        }
+
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return false;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        /*/// <summary>
         /// 摄像头推流的时候
         /// </summary>
         /// <param name="client"></param>
@@ -246,10 +389,54 @@ namespace SrsApis.SrsManager.Apis
 
                 return false;
             }
-        }
-
+        }*/
 
         /// <summary>
+        /// 摄像头停止推流的时候
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public static bool OnUnPublish(Client client)
+        {
+            lock (Common.LockObj)
+            {
+                if (client != null && !string.IsNullOrEmpty(client.Device_Id) && client.Client_Id != null)
+                {
+                    try
+                    {
+                        var ret = OrmService.Db.Update<Client>()
+                            .Set(x => x.IsOnline, false).Set(x => x.UpdateTime, client.UpdateTime)
+                            .Where(x => x.Client_Id == client.Client_Id &&
+                                        x.Device_Id!.Trim() == client.Device_Id.Trim())
+                            .ExecuteAffrows();
+
+                        if (ret <= 0)
+                        {
+                            client.ClientType = ClientType.Monitor;
+                            client.IsOnline = false;
+                            var retInsert = OrmService.Db.Insert(client).ExecuteAffrows();
+                            if (retInsert > 0)
+                            {
+                                return true;
+                            }
+
+                            return false;
+                        }
+
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return false;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        /*/// <summary>
         /// 摄像头停止推流的时候
         /// </summary>
         /// <param name="client"></param>
@@ -295,11 +482,60 @@ namespace SrsApis.SrsManager.Apis
 
                 return false;
             }
+        }*/
+
+
+        /// <summary>
+        /// 当有设备连接时
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="rs"></param>
+        /// <returns></returns>
+        public static bool OnConnect(Client client)
+        {
+            lock (Common.LockObj)
+            {
+                if (client != null && !string.IsNullOrEmpty(client.Device_Id) && client.Client_Id != null)
+                {
+                    try
+                    {
+                        var ret = OrmService.Db.Update<Client>().Set(x => x.ClientType, ClientType.Monitor)
+                            .Set(x => x.IsOnline, true).Set(x => x.HttpUrl, client.HttpUrl)
+                            .Set(x => x.Param, client.Param)
+                            .Set(x => x.Stream, client.Stream).Set(x => x.UpdateTime, client.UpdateTime)
+                            .Set(x => x.MonitorIp, client.ClientIp)
+                            .Where(x => x.Client_Id == client.Client_Id &&
+                                        x.Device_Id!.Trim() == client.Device_Id.Trim())
+                            .ExecuteAffrows();
+                        if (ret <= 0)
+                        {
+                            client.ClientType = ClientType.Monitor;
+                            client.IsOnline = true;
+                            client.MonitorIp = client.ClientIp;
+                            client.MonitorType = MonitorType.Unknow;
+                            var retInsert = OrmService.Db.Insert(client).ExecuteAffrows();
+                            if (retInsert > 0)
+                            {
+                                return true;
+                            }
+
+                            return false;
+                        }
+
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return false;
+                    }
+                }
+
+                return false;
+            }
         }
 
-        
-        
-
+        /*
         /// <summary>
         /// 当有设备连接时
         /// </summary>
@@ -352,6 +588,6 @@ namespace SrsApis.SrsManager.Apis
 
                 return false;
             }
-        }
+        }*/
     }
 }
